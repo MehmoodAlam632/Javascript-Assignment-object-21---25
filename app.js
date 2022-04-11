f58012502ec0d12b64be4c88adee6548ba308f63
mehmood <mehmoodalam60632@gmail.com>
2022-04-12 04:47:00 +0500

    Add question

--- app.js
+++ app.js
@@ -11,9 +11,21 @@
     //________________Question.2________________//
 // var user = prompt("Enter your favourite Mobile Name");
 // document.write("My Favourite Phone is: " + user);
+
+    //________________Question.3________________//
+// var a = "Pakistani";
+// var b = a.indexOf("n");
+// document.write("String: " + a + "<br/>");
+// document.write("Index of 'n': " + b);
+
+    //________________Question.4________________//
+// var a = "Hello World";
+// var b = a.lastIndexOf("l");
+// document.write("String: " + a + "<br/>");
+// document.write("Last index of 'l': " + b);
+
     //________________Question.1________________//
-    //________________Question.1________________//
-    //________________Question.1________________//
+
     //________________Question.1________________//
     //________________Question.1________________//
     //________________Question.1________________//
