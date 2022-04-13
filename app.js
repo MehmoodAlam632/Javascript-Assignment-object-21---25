91f677026fed587f9c8547da7b9db9362e2e7c48
mehmood <mehmoodalam60632@gmail.com>
2022-04-14 03:46:57 +0500

    Add code

--- app.js
+++ app.js
@@ -1,48 +1,48 @@
 // alert("Assignment");
 
-        //Chap # 21-25
-    
-    //________________Question.1________________//
+//Chap # 21-25
+
+//________________Question.1________________//
 // var firstName = prompt("Enter your first Name");
 // var lastName =  prompt("Enter your Last Name");
 // var fullName = firstName + lastName;
 // alert("Hello " + fullName);
 
-    //________________Question.2________________//
+//________________Question.2________________//
 // var user = prompt("Enter your favourite Mobile Name");
 // document.write("My Favourite Phone is: " + user);
 
-    //________________Question.3________________//
+//________________Question.3________________//
 // var a = "Pakistani";
 // var b = a.indexOf("n");
 // document.write("String: " + a + "<br/>");
 // document.write("Index of 'n': " + b);
 
-    //________________Question.4________________//
+//________________Question.4________________//
 // var a = "Hello World";
 // var b = a.lastIndexOf("l");
 // document.write("String: " + a + "<br/>");
 // document.write("Last index of 'l': " + b);
 
-    //________________Question.5________________//
+//________________Question.5________________//
 // var country = "Pakistani";
 // var b = country.charAt(3);
 // document.write("String: " + country + "<br/> Character at index 3: " + b );
 
-    //________________Question.6________________//
+//________________Question.6________________//
 
-    //________________Question.7________________//
+//________________Question.7________________//
 // var city = "Hyderabad";
 // var city2 = city.replace(/Hyderabad/, "Islamabad");
 // document.write("City: " + city + "<br/>" + "After replacement: " + city2);
 
-    //________________Question.8________________//
+//________________Question.8________________//
 // var massage = "Ali and Sami are best friends. They play cricket and football together.";
 // var msg = massage.replace(/and/g, "&");
 // document.write(massage + "<br/>");
 // document.write(msg);
 
-    //________________Question.9________________//
+//________________Question.9________________//
 // var str = "472";
 // var num = 472;
 // document.write("Value: " +  str + "<br/>");
@@ -50,24 +50,30 @@
 // document.write("Value: " +  num + "<br/>");
 // document.write("Types of : " + typeof num);
 
-    //________________Question.10________________//
+//________________Question.10________________//
 // var user = prompt("Enter your input");
 // var user2 = user.toUpperCase();
 // document.write("User input : " + user + "<br/>");
 // document.write("Upper case : " + user2);
 
-    //________________Question.11________________//
+//________________Question.11________________//
+// var string = prompt("Enter your text");
+// document.write("User input : " +  string + "<br/>");
+// var user = string.toUpperCase();
+// document.write("Tittle case : " + user.charAt(0).toUpperCase() + user.slice(1).toLowerCase());
 
-    //________________Question.12________________//
-    //________________Question.1________________//
-    //________________Question.1________________//
-    //________________Question.1________________//
-    //________________Question.1________________//
-    //________________Question.1________________//
-    //________________Question.1________________//
-    //________________Question.1________________//
-    //________________Question.1________________//
-    //________________Question.1________________//
-    //________________Question.1________________//
-    //________________Question.1________________//
-    //________________Question.1________________//
+//________________Question.12________________//
+// var num = 35.36;
+// console.log(typeof num);
+// num = String(num);
+// console.log(typeof num);
+// number = num.splice(2, 1);
+// console.log(number);
+
+
+// var fruits = ["Banana", "Orange", "Apple", "Mango"];
+// fruits.splice(2, 0, "Lemon", "Kiwi");
+// console.log(fruits);
+// OUTPUT: Banana,Orange,Lemon,Kiwi,Ap
+
+    
